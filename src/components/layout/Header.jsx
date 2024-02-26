02a0005f774803692611d568fcd9f99b1cef1b8f
kewii33 <kewiii071@gmail.com>
2024-02-26 19:14:08 +0900

    [feat]유튜브 api를 이용한 검색기능 추가

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,26 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
+import Select from 'react-select';
 import { HeaderStyle, StImg, SearchBox, SearchInput, SearchButton, StButton } from 'styles/HeaderStyles';
 import { FaMagnifyingGlass } from 'react-icons/fa6';
 import logoandtitle from '../../assets/imgs/logoandtitle.png';
 
+const searchOptions = [
+  { value: 'reactube', label: 'reactube' },
+  { value: 'youtube', label: 'youtube' }
+];
+
 function Header() {
   const navigate = useNavigate();
   const [searchTerm, setSearchTerm] = useState('');
+  const [selectedSearchOption, setSelectedSearchOption] = useState(searchOptions[0]);
+  const [youtubeResults, setYoutubeResults] = useState([]);
 
-  const handleSearchInfo = (e) => {
+  const handleSearchInfo = async (e) => {
     e.preventDefault();
     if (!searchTerm.trim()) {
       alert('검색어를 입력해주세요!');
       return;
     }
-    navigate(`/search?keyword=${searchTerm}`);
+
+    if (selectedSearchOption.value === 'reactube') {
+      navigate(`/search?keyword=${searchTerm}`);
+    } else if (selectedSearchOption.value === 'youtube') {
+      navigate(`/search/${searchTerm}`);
+    }
+
     setSearchTerm('');
   };
+
+  const handleChangeSearchOption = (selectedOption) => {
+    setSelectedSearchOption(selectedOption);
+  };
+
+  useEffect(() => {
+    console.log('YouTube 검색 결과:', youtubeResults);
+  }, [youtubeResults]);
+
   return (
     <>
       <HeaderStyle>
         <StImg src={logoandtitle} alt="Logo" onClick={() => navigate('/home')} />
+        <Select
+          value={selectedSearchOption}
+          onChange={handleChangeSearchOption}
+          options={searchOptions}
+          isSearchable={false}
+        />
         <SearchBox onSubmit={handleSearchInfo}>
           <SearchInput
             value={searchTerm}
@@ -42,7 +71,7 @@ function Header() {
           </Link>
         </div>
       </HeaderStyle>
-      <Outlet></Outlet>
+      <Outlet />
     </>
   );
 }
