e63316b1b08e213aef287a263324c27046da8b9b
jisu <ahteostnqls@naver.com>
2024-02-27 01:58:44 +0900

    [Feat] 메인 리스트 lazy load 적용

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,28 +1,50 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 import { ButtonArea, AddVideoButton, Container, Videos, VideoCard } from 'styles/VideoListStyle.js';
 import { formatAgo } from 'util/date';
 import { loadBoardItems } from '../redux/modules/boardSlice';
+import { LazyLoadedImage } from './LazyLoadedImage';
 
 function VideoList() {
   const boardItems = useSelector((state) => state.boardItems.boardItems);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [visibleIndices, setVisibleIndices] = useState([]);
+  const imageRefs = useRef([]);
 
   useEffect(() => {
     dispatch(loadBoardItems());
   }, [dispatch]);
 
-  const detailViewer = (event) =>
-    navigate(`/detail/${event.target.id}`, {
-      state: { id: event.target.id }
+  const detailViewer = (id) =>
+    navigate(`/detail/${id}`, {
+      state: { id }
     });
 
   const newVideo = () => {
     navigate('/write');
   };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const index = imageRefs.current.indexOf(entry.target);
+          setVisibleIndices((prevVisibleIndices) => [...prevVisibleIndices, index]);
+        }
+      });
+    });
+
+    imageRefs.current.forEach((ref) => {
+      observer.observe(ref);
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [boardItems]);
 
   return (
     <>
@@ -31,20 +53,21 @@ function VideoList() {
       </ButtonArea>
       <Container>
         <Videos>
-          {boardItems.map((element) => {
+          {boardItems.map((element, index) => {
+            const src = element.videoId ? `https://img.youtube.com/vi/${element.videoId}/maxresdefault.jpg` : '';
             return (
-              <VideoCard key={element.id} id={element.id} onClick={detailViewer}>
-                <img
-                  id={element.id}
-                  onClick={detailViewer}
-                  src={`https://img.youtube.com/vi/${element.videoId}/maxresdefault.jpg`}
-                  alt="Thumbnail"
-                />
+              <VideoCard
+                key={element.id}
+                id={element.id}
+                onClick={() => detailViewer(element.id)}
+                ref={(e) => (imageRefs.current[index] = e)}
+              >
+                {visibleIndices.includes(index) && <LazyLoadedImage src={src} alt={`${element.title} 썸네일`} />}
                 <div>
-                  <p className="title" id={element.id} onClick={detailViewer}>
+                  <p className="title" id={element.id} onClick={() => detailViewer(element.id)}>
                     {element.title}
                   </p>
-                  <p className="author" id={element.id} onClick={detailViewer}>
+                  <p className="author" id={element.id} onClick={() => detailViewer(element.id)}>
                     by. {element.author}
                   </p>
                   <p className="timeString">{formatAgo(element.timeString, 'ko')}</p>
