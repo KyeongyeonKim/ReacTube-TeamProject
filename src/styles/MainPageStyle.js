121c8067469fa70e3017f6d2b942ae2ac0b0cfc8
YeonjinJo <hrj8441@naver.com>
2024-02-27 14:40:47 +0900

    [PATCH] Main Page Modification

--- src/styles/MainPageStyle.js
+++ src/styles/MainPageStyle.js
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
 
 export const PageLayout = styled.div`
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
   background-color: darkgray;
+  position: absolute;
+  top: 0;
+  left: 0;
 `;
 
 export const Container = styled.div`
