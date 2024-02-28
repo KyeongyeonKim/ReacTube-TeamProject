65ea53575d5fc4fb571760169c800aad40340659
YeonjinJo <hrj8441@naver.com>
2024-02-28 09:02:12 +0900

    [PATCH} Delete button modification

--- src/components/modals/DeleteModal.jsx
+++ src/components/modals/DeleteModal.jsx
@@ -8,9 +8,7 @@ import {
   ModalContainer,
   ModalForm,
   StyledButton,
-  StyledInput,
-  DeleteModalTitle,
-  StyledSection
+  DeleteModalTitle
 } from 'styles/modalStyles/DeleteModalStyle';
 
 function DeleteModal(props) {
@@ -20,45 +18,28 @@ function DeleteModal(props) {
 
   const target = boardItems.filter((element) => id === element.id);
   const dispatch = useDispatch();
-  const [password, setPassword] = useState('');
-  const passwordRef = useRef();
 
   const deleteForm = () => {
-    if (window.confirm('취소하시겠습니까?')) {
-      props.setDeleteOpen(!props.deleteOpen);
-    }
+    props.setDeleteOpen(!props.deleteOpen);
   };
 
   const backToList = () => {
     navigate('/home', { replace: true });
   };
 
-  const onChange = (event) => {
-    setPassword(event.target.value);
-  };
-
   const onSubmitHandler = async (event) => {
     event.preventDefault();
-    if (password === target[0].password) {
-      if (window.confirm('정말로 삭제하시겠습니까?')) {
-        try {
-          const { error } = await client.from('content').delete().eq('id', id);
-          if (error) {
-            throw error;
-          }
-          dispatch(deleteBoard(...target));
-          backToList();
-          alert('게시물이 삭제되었습니다.');
-        } catch (error) {
-          console.error('게시물 삭제 중 오류가 발생', error.message);
-          alert('게시물 삭제 중 오류가 발생했습니다.');
-        }
-      } else {
-        alert('삭제가 취소되었습니다.');
+    try {
+      const { error } = await client.from('content').delete().eq('id', id);
+      if (error) {
+        throw error;
       }
-    } else {
-      alert('비밀번호가 일치하지 않습니다!');
-      passwordRef.current.focus();
+      dispatch(deleteBoard(...target));
+      backToList();
+      alert('게시물이 삭제되었습니다.');
+    } catch (error) {
+      console.error('게시물 삭제 중 오류가 발생', error.message);
+      alert('게시물 삭제 중 오류가 발생했습니다.');
     }
   };
 
@@ -67,18 +48,6 @@ function DeleteModal(props) {
       <ModalContainer onClick={deleteForm} />
       <ModalForm onSubmit={onSubmitHandler}>
         <DeleteModalTitle>삭제하시겠습니까?</DeleteModalTitle>
-        <StyledSection>
-          <StyledInput
-            id={id + 'password'}
-            type="password"
-            name="password"
-            value={password}
-            ref={passwordRef}
-            placeholder="비밀번호 확인"
-            minLength={6}
-            onChange={onChange}
-          />
-        </StyledSection>
         <Buttons>
           <StyledButton onClick={onSubmitHandler}>확인</StyledButton>
           <StyledButton onClick={deleteForm}>취소</StyledButton>
